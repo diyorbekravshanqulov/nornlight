d1307699102c793393162d679496a4647aaf12d8
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-07-03 23:44:09 +0500

    in process

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container pt-3">
+  <div class="container md:pt-3">
     <div
       class="max-md:hidden flex w-full justify-between mb-2 text-primary font-semibold text-sm"
     >
@@ -23,18 +23,19 @@
     :class="isScrolled ? 'backdrop-blur-lg bg-gradient-to-r' : 'bg-white'"
   >
     <div class="container">
+      <div>
       <div class="flex w-full items-center justify-between gap-4">
         <div class="flex gap-[29px]">
           <img src="/MainLogo.svg" class="cursor-pointer" alt="Logo" />
           <div
             @click="dropdownClick = !dropdownClick"
-            class="flex px-[27px] gap-[9px] cursor-pointer items-center rounded-full bg-primary py-[14px]"
+            class="flex max-md:hidden px-[27px] gap-[9px] cursor-pointer items-center rounded-full bg-primary py-[14px]"
           >
             <img src="/catalog.svg" alt="category" />
             <p class="text-white font-semibold">Каталог</p>
           </div>
         </div>
-        <div class="relative w-[45%]">
+        <div class="relative max-md:hidden w-[45%]">
           <input
             class="font-semibold bg-white/50 outline-none pl-[27px] pr-12 h-full py-[14px] placeholder:text-primary border border-primary rounded-full w-full"
             type="text"
@@ -47,7 +48,7 @@
           />
         </div>
         <div
-          class="flex h-full items-center"
+          class="flex h-full items-center max-md:hidden"
           v-for="(item, index) in image"
           :key="index"
         >
@@ -58,10 +59,38 @@
               :class="index + 1 != image.length ? 'w-1/3' : 'w-1/2'"
               alt="Logos "
             />
-            <p class="text-[12px] font-semibold text-primary">
+            <p class="text-[12px] max-md:hidden font-semibold text-primary">
               {{ bottomSections[index] }}
             </p>
           </div>
+          
+        </div>
+        
+         <div
+          class="flex h-full md:hidden items-center "
+          v-for="(item, index) in image2"
+          :key="index"
+        >
+            <img
+              :src="item"
+              class="cursor-pointer"
+              :class="index + 1 != image.length ? 'w-full' : 'w-[70%]'"
+              alt="Logos "
+            />
+
+        </div>
+        </div>
+        <div class="relative md:hidden w-[100%] mt-2">
+          <input
+            class="font-semibold text-sm bg-white/50 outline-none pl-[27px] pr-12 h-full placeholder:text-primary border py-[10px] border-primary rounded-full w-full"
+            type="text"
+            placeholder="Поиск по товарам"
+          />
+          <img
+            class="absolute top-1/2 -translate-y-1/2 right-0 -translate-x-3/4 z-10"
+            src="/search.svg"
+            alt="search"
+          />
         </div>
       </div>
     </div>
@@ -82,6 +111,7 @@ const sections = ref([
 
 const bottomSections = ref(["Избранное", "Сравнение", "Корзина"]);
 const image = ref(["/like5.svg", "/compare.svg", "/cart.svg"]);
+const image2 = ref(["/like5.svg", "/cart.svg"]);
 
 const isScrolled = ref(false);
 
